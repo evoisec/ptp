be7d5bdedb28d7c74c49457fd583b29aee67ad5c
Evo <evo@isecc.com>
2020-01-01 11:56:07 -0500

    s

--- sptest.py
+++ sptest.py
@@ -1,5 +1,22 @@
-from pyspark import SparkConf, SparkContext
+#from pyspark import SparkConf, SparkContext
 
-sc = SparkContext(master="local",appName="Spark Demo")
-print(sc.textFile("/opt/citizens-del.json").first())
-print(sc.textFile("/opt/citizens-del.json").count())
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, asc
+from pyspark.sql import functions as F
+from pyspark.sql.types import *
+
+spark = SparkSession.builder.appName("CLIENT-ETL-Pipeline").getOrCreate()
+
+df1 = spark.read.csv("file:/root/PycharmProjects/ptp/table1.csv", header=True, inferSchema=True)
+#df2 = spark.read.csv("/opt/projects/1/table2.csv", header=True)
+
+df1.show()
+df1.printSchema()
+
+df4 = df1.createOrReplaceTempView("Table1")
+df5 = spark.sql("Select * from Table1 where c1=12")
+df5.show()
+
+df5.write.csv("file:/root/PycharmProjects/ptp/table1-deriv.csv", header=True)
+
+spark.stop()
