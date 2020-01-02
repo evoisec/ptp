d5a87968a0848990ff22144d99a8e07d7c1572d5
Evo <evo@isecc.com>
2020-01-02 14:36:21 -0500

    s

--- sptest.py
+++ sptest.py
@@ -1,9 +1,14 @@
 #from pyspark import SparkConf, SparkContext
+import sys
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, asc
+#from pyspark.sql.functions import col, asc
 from pyspark.sql import functions as F
 from pyspark.sql.types import *
+
+print(sys.version)
+print(sys.version_info)
+#sys.exit(0)
 
 spark = SparkSession.builder.appName("CLIENT-ETL-Pipeline").getOrCreate()
 
@@ -17,6 +22,6 @@ df4 = df1.createOrReplaceTempView("Table1")
 df5 = spark.sql("Select * from Table1 where c1=12")
 df5.show()
 
-df5.write.csv("file:/root/PycharmProjects/ptp/table1-deriv.csv", header=True)
+df5.write.mode("overwrite").csv("file:/root/PycharmProjects/ptp/table1-deriv.csv", header=True)
 
 spark.stop()
