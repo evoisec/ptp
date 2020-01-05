ab62a3b94ce6d729a7212a66e56ef0a4f08afe92
Evo <evo@isecc.com>
2020-01-05 13:15:24 -0500

    s

---
+++ gmm.py
@@ -0,0 +1,29 @@
+"""
+Gaussian Mixture Model (GMM).
+
+"""
+from __future__ import print_function
+
+# $example on$
+from pyspark.ml.clustering import GaussianMixture
+# $example off$
+from pyspark.sql import SparkSession
+
+if __name__ == "__main__":
+    spark = SparkSession\
+        .builder\
+        .appName("GaussianMixture")\
+        .getOrCreate()
+
+    # $example on$
+    # loads data
+    dataset = spark.read.format("libsvm").load("file:/opt/data/gmm-data.txt")
+
+    gmm = GaussianMixture().setK(2).setSeed(538009335)
+    model = gmm.fit(dataset)
+
+    print("Gaussians shown as a DataFrame: ")
+    model.gaussiansDF.show(truncate=False)
+    # $example off$
+
+    spark.stop()
