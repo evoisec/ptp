311921b4022c3bc43c455b0ae4c61f35705605e4
Evo <evo@isecc.com>
2020-01-05 15:12:22 -0500

    s

--- dsgen2.py
+++ dsgen2.py
@@ -5,6 +5,7 @@ seed(1)
 
 file = open("/opt/data/testfile.txt", "w")
 
+#for x in range(11, 100): (do on a seperate thread or process / shell for the next segment)
 for x in range(10):
 
     file.write(str(x) + " 1:" + str(random()) + " 2:" + str(random()) + " 3:" + str(random()) + "\n")
