e448d87aea06e4cc554c81cfe9da9236d8299afd
Evo <evo@isecc.com>
2020-01-05 15:10:11 -0500

    s

--- dsgen2.py
+++ dsgen2.py
@@ -1,7 +1,12 @@
+from random import seed
+from random import random
+
+seed(1)
+
 file = open("/opt/data/testfile.txt", "w")
 
 for x in range(10):
 
-    file.write(str(x) + " 1:0.0 2:0.0 3:0.0\n")
+    file.write(str(x) + " 1:" + str(random()) + " 2:" + str(random()) + " 3:" + str(random()) + "\n")
 
 file.close()
