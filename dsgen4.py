3110212ee447e8da1d433861f032e00e89af0a77
Evo <evo@isecc.com>
2020-01-13 10:00:57 -0500

    s

---
+++ dsgen4.py
@@ -0,0 +1,42 @@
+import psycopg2
+from psycopg2 import Error
+
+###################################################################
+#
+# tool to create the schema for performance testing in PostgressSQL
+#
+###################################################################
+
+try:
+    connection = psycopg2.connect(user="evo",
+                                  password="Pwd1234",
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="perftesting")
+
+    cursor = connection.cursor()
+    # Print PostgreSQL Connection properties
+    print ( connection.get_dsn_parameters(),"\n")
+
+    # Print PostgreSQL version
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print("You are connected to - ", record,"\n")
+
+    create_table_query = '''CREATE TABLE IF NOT EXISTS CITIZEN
+          (NIN INT PRIMARY KEY     NOT NULL,
+          NAME           TEXT    NOT NULL,
+          BENEFITS         REAL); '''
+
+    cursor.execute(create_table_query)
+    connection.commit()
+    print("Table created successfully in PostgreSQL ")
+
+except (Exception, psycopg2.DatabaseError) as error:
+    print("Error while creating PostgreSQL table", error)
+finally:
+    # closing database connection.
+    if (connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
