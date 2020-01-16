bf2a5082d13f35ae4157f90b62d0cd739061c4cb
Evo <evo@isecc.com>
2020-01-16 10:00:44 -0500

    s

--- dsgen2.py
+++ dsgen2.py
@@ -1,5 +1,6 @@
 from random import seed
 from random import random
+import uuid
 
 # Generates synthetic data in the standard LIBSVM data format specifically intended for Machine Learning
 
@@ -8,8 +9,10 @@ seed(1)
 file = open("/opt/data/testfile.txt", "w")
 
 #for x in range(11, 100): (do on a seperate thread or process / shell for the next segment)
-for x in range(1000):
+for x in range(100):
 
-    file.write(str(x) + " 1:" + str(random()) + " 2:" + str(random()) + " 3:" + str(random()) + "\n")
+    #file.write(str(x) + " 1:" + str(random()) + " 2:" + str(random()) + " 3:" + str(random()) + "\n")
+
+    file.write(str(str(uuid.uuid4().int)) + " 1:" + str(random()) + " 2:" + str(random()) + " 3:" + str(random()) + "\n")
 
 file.close()
