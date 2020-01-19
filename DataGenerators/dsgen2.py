e0fc2b613e6541c9359f702da5a90b92da3df1a4
Evo <evo@isecc.com>
2020-01-19 15:12:55 -0500

    s

--- DataGenerators/dsgen2.py
+++ DataGenerators/dsgen2.py
@@ -1,12 +1,12 @@
 from random import seed
-from random import random
+from random import *
 import uuid
 
 # Generates synthetic data in the standard LIBSVM data format specifically intended for Machine Learning
 
 seed(1)
 
-file = open("/opt/data/testfile.txt", "w")
+file = open("/root/PycharmProjects/ptp/Data/classification-2.txt", "w")
 
 #for x in range(11, 100): (do on a seperate thread or process / shell for the next segment)
 for x in range(100):
@@ -15,6 +15,9 @@ for x in range(100):
     #file.write(str(x) + " 1:" + str(random()) + " 2:" + str(random()) + " 3:" + str(random()) + "\n")
 
     # generates labels for Feature Vectors in the form of unique numbers, which are gurateed to be unique through cryptographic means
-    file.write(str(str(uuid.uuid4().int)) + " 1:" + str(random()) + " 2:" + str(random()) + " 3:" + str(random()) + "\n")
+    #file.write(str(str(uuid.uuid4().int)) + " 1:" + str(random()) + " 2:" + str(random()) + " 3:" + str(random()) + "\n")
+
+    file.write("0" + " 1:" + str(gauss(1,0.1)) + " 2:" + str(gauss(5,0.5)) + " 3:" + str(gauss(9,1)) + "\n")
+    file.write("1" + " 1:" + str(gauss(11, 1)) + " 2:" + str(gauss(3, 0.2)) + " 3:" + str(gauss(9, 1)) + "\n")
 
 file.close()
