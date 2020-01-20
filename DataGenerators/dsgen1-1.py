7ca6deb1d4d79e2649c6cd88b868bf8255d795a8
Evo <evo@isecc.com>
2020-01-20 07:52:33 -0500

    s

--- DataGenerators/dsgen1-1.py
+++ DataGenerators/dsgen1-1.py
@@ -13,12 +13,30 @@ from sklearn.datasets import make_moons, make_circles, make_classification
 # Supports the follwoing Data Science Workloads:
 # LogisticRegression.py
 
-n_samples = 1000
+Config = {}
 
-X, y = make_classification(n_features=2, n_redundant=0, n_informative=2,
-                           random_state=1, n_clusters_per_class=1)
+f = open('datagen1.cfg', 'r')
+line = f.readline()
+while (line != ""):
+    line = line.rstrip()
+    x = line.split('=')
+    print(x[0])
+    print(x[1])
+    Config[x[0]] = x[1]
+    line = f.readline()
+
+n_samples = int(Config['number.samples'])
+n_features = int(Config['number.features'])
+n_informative = int(Config['number.informative'])
+noise = int(Config['noise'])
+random_state = int(Config['random.state'])
+file=Config['file']
+
+
+X, y = make_classification(n_samples=n_samples, n_features=n_features, n_redundant=0, n_informative=n_informative,
+                           random_state=random_state, n_clusters_per_class=1)
 
 print(X)
 print(y)
 
-dump_svmlight_file(X, y, "/opt/data/classification.txt", zero_based=False)
+dump_svmlight_file(X, y, file, zero_based=False)
