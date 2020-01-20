40e525a888d4e6769b42ef805987d06eb08000b7
Evo <evo@isecc.com>
2020-01-20 08:33:57 -0500

    s

--- gmm.py
+++ gmm.py
@@ -10,6 +10,23 @@ from pyspark.ml.clustering import GaussianMixture
 from pyspark.sql import SparkSession
 
 if __name__ == "__main__":
+
+    Config = {}
+
+    f = open('DecisionTree.cfg', 'r')
+    line = f.readline()
+    while (line != ""):
+        line = line.rstrip()
+        x = line.split('=')
+        print(x[0])
+        print(x[1])
+        Config[x[0]] = x[1]
+        line = f.readline()
+
+    file = Config['file']
+    trainRatio = float(Config['train.ratio'])
+    testRatio = float(Config['test.ratio'])
+
     spark = SparkSession\
         .builder\
         .appName("GaussianMixture")\
@@ -20,9 +37,9 @@ if __name__ == "__main__":
     # dataset = spark.read.format("libsvm").load("file:/opt/data/gmm-data.txt/gmm-data.txt")
     # dataset = spark.read.format("libsvm").load("file:/opt/data/gmm-data.txt")
     # dataset = spark.read.format("libsvm").load("file:/opt/data/testfile.txt")
-    dataset = spark.read.format("libsvm").load("file:/root/PycharmProjects/ptp/Data/gmm.txt")
+    dataset = spark.read.format("libsvm").load(file)
 
-    dataset.show(100)
+    #dataset.show(100)
 
     gmm = GaussianMixture().setK(2).setSeed(538009335)
     model = gmm.fit(dataset)
