07abeb52335f6d8c22f1f7b969c6b1c80725fe1d
Evo <evo@isecc.com>
2020-01-20 07:07:16 -0500

    s

--- DataGenerators/dsgen3.py
+++ DataGenerators/dsgen3.py
@@ -42,6 +42,14 @@ from random import *
 #kerb_ticket=kerb_ticket
 fs = pa.hdfs.connect("localhost", user="cloudera")
 
+i =1
+a = 2
+b = 4
+c = 5
+d = 7
+
+mlType = "regression"
+
 # hdfs file access modes: rb, wb, ab
 # open and write to hdfs file
 with fs.open("/user/cloudera/synt/syntdata.csv", 'wb') as f:
@@ -54,8 +62,21 @@ with fs.open("/user/cloudera/synt/syntdata.csv", 'wb') as f:
         # generates labels for Feature Vectors in the form of unique numbers, which are gurateed to be unique through cryptographic means
         #f.write((str(uuid.uuid4().int) + ' 1:' + str(random.random()) + ' 2:' + str(random.random()) + ' 3:' + str(random.random()) + '\n').encode('UTF-8'))
 
-        f.write("0" + " 1:" + str(gauss(1, 0.1)) + " 2:" + str(gauss(5, 0.5)) + " 3:" + str(gauss(9, 1)) + "\n")
-        f.write("1" + " 1:" + str(gauss(11, 1)) + " 2:" + str(gauss(3, 0.2)) + " 3:" + str(gauss(9, 1)) + "\n")
+        if (mlType.lower() == "classification"):
+            f.write( ( "0" + " 1:" + str(gauss(1, 0.1)) + " 2:" + str(gauss(5, 0.5)) + " 3:" + str(gauss(9, 1)) + " 4:" + str(gauss(21, 2)) + "\n").encode('UTF-8') )
+            f.write( ( "1" + " 1:" + str(gauss(11, 1)) + " 2:" + str(gauss(3, 0.2)) + " 3:" + str(gauss(9, 1)) + " 4:" + str(gauss(17, 0.2)) + "\n").encode('UTF-8') )
+            f.write( ( "3" + " 1:" + str(gauss(11, 1)) + " 2:" + str(gauss(7, 0.2)) + " 3:" + str(gauss(44, 1)) + " 4:" + str(gauss(33, 1)) + "\n").encode('UTF-8') )
+
+        if (mlType.lower() == "regression"):
+            x1 = float("{0:.2f}".format(uniform(1, 3)))
+            x2 = float("{0:.2f}".format(uniform(5, 8)))
+            x3 = float("{0:.2f}".format(uniform(12, 18)))
+            x4 = float("{0:.2f}".format(uniform(1, 6)))
+            noise = float("{0:.2f}".format(gauss(1, 0.1)))
+            y = i + a * x1 + b * x2 + c * x3 + d * x4 + noise
+            y = float("{0:.2f}".format(y))
+
+            f.write( ( str(y) + " 1:" + str(x1) + " 2:" + str(x2) + " 3:" + str(x3) + " 4:" + str(x4) + "\n").encode('UTF-8') )
 
 fs.close()
 
