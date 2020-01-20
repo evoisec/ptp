aced0fc442a5c452a8bc186b23caf801423fbf44
Evo <evo@isecc.com>
2020-01-20 08:54:02 -0500

    s

--- RandomForest.py
+++ RandomForest.py
@@ -10,21 +10,40 @@ from pyspark.mllib.util import MLUtils
 # $example off$
 
 if __name__ == "__main__":
+
+    Config = {}
+
+    f = open('RandomForest.cfg', 'r')
+    line = f.readline()
+    while (line != ""):
+        line = line.rstrip()
+        x = line.split('=')
+        print(x[0])
+        print(x[1])
+        Config[x[0]] = x[1]
+        line = f.readline()
+
+    file = Config['file']
+    trainRatio = float(Config['train.ratio'])
+    testRatio = float(Config['test.ratio'])
+    maxDepth = int(Config['max.depth'])
+    numTrees = int(Config['number.trees'])
+
     sc = SparkContext(appName="RandomForestRegression")
     # $example on$
     # Load and parse the data file into an RDD of LabeledPoint.
     # data = MLUtils.loadLibSVMFile(sc, "file:/root/PycharmProjects/ptp/Data/classification.txt")
-    data = MLUtils.loadLibSVMFile(sc, "file:/root/PycharmProjects/ptp/Data/regression.txt")
+    data = MLUtils.loadLibSVMFile(sc, file)
     # Split the data into training and test sets (30% held out for testing)
-    (trainingData, testData) = data.randomSplit([0.7, 0.3])
+    (trainingData, testData) = data.randomSplit([trainRatio, testRatio])
 
     # Train a RandomForest model.
     #  Empty categoricalFeaturesInfo indicates all features are continuous.
     #  Note: Use larger numTrees in practice.
     #  Setting featureSubsetStrategy="auto" lets the algorithm choose.
     model = RandomForest.trainRegressor(trainingData, categoricalFeaturesInfo={},
-                                        numTrees=3, featureSubsetStrategy="auto",
-                                        impurity='variance', maxDepth=4, maxBins=32)
+                                        numTrees=numTrees, featureSubsetStrategy="auto",
+                                        impurity='variance', maxDepth=maxDepth, maxBins=32)
 
     # Evaluate model on test instances and compute test error
     predictions = model.predict(testData.map(lambda x: x.features))
