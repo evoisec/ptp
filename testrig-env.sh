4515a7beba0ec2b53dc3fa146cf36eac83a69be1
Evo <evo@isecc.com>
2020-01-22 05:16:19 -0500

    s

---
+++ testrig-env.sh
@@ -0,0 +1,6 @@
+export ARROW_LIBHDFS_DIR=/opt/cloudera/parcels/CDH-5.16.1-1.cdh5.16.1.p0.3/lib64/
+
+export PYSPARK_PYTHON=/opt/anaconda3/bin/python3
+export PYSPARK_DRIVER_PYTHON=/opt/anaconda3/bin/python3
+
+export PATH=/opt/anaconda3/bin:$PATH
